bdb3793a43773c76b7104f323a1ba8077403b68d
adavis2010 <78383492+adavis2010@users.noreply.github.com>
2021-03-25 09:51:17 -0400

    commit

--- friends.js
+++ friends.js
@@ -1,18 +1,35 @@
-let names = [];
+
     
    
+let friends = []; //array with no data in it
+    
 
-    const loadDiv = () => {
-       let divCtrl = document.getElementById("input");
-        divCtrl.innerHTML= "";
-       if(names!=null && names!= ""){
-
-       }
-        let p = `<p>${insert.name}</p>`; //used back tick
-        divCtrl.innerHTML += p;
-        ctrl.style.color = "blue";
-        ctrl.style.fontsize = "14pt";
-       }
+const addFriend = () => {
+    let fname = document.getElementById("iname").value; 
+    let femail = document.getElementById("iemail").value;
+    let fphone = document.getElementById("iphone").value;
+    let friend = { //friend variable
+        name:fname, email:femail, phone:fphone
+    }
+    friends.push(friend); //push friend into the array
+    display();
+}
+   
+const display = () => { // create display function
+    let tbody = document.getElementById("tbody");
+    tbody.innerHTML = "";
+    for(let friend of friends){
+        let tr = "<tr>";
+        tr +=`<td>${friend.name}</td>`;
+        tr +=`<td>${friend.email}</td>`;
+        tr +=`<td>${friend.phone}</td>`;
+        tr += "</tr>";
+        tbody.innerHTML += tr; //Append tr tag to what is currently in the body
+    }
+}
+        
+        
+       
     
 
 
