16299a4435965d9e2bc9cefe694defb09744ce1c
adavis2010 <78383492+adavis2010@users.noreply.github.com>
2021-03-24 21:17:31 -0400

    updates

--- friends.js
+++ friends.js
@@ -5,14 +5,16 @@ let names = [];
     const loadDiv = () => {
        let divCtrl = document.getElementById("input");
         divCtrl.innerHTML= "";
-       for(let name of names){ //js foreach loop
+       if(names!=null && names!= ""){
+
+       }
         let p = `<p>${insert.name}</p>`; //used back tick
         divCtrl.innerHTML += p;
         ctrl.style.color = "blue";
         ctrl.style.fontsize = "14pt";
        }
-    }
+    
 
 
 
-}     
+    
